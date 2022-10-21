1b3aa492f3357307acb053bd3dd02ad5a039a9be
zoli0324 <zoli0324@gmail.com>
2022-10-21 09:47:39 +0200

    add spring basic files

---
+++ src/main/java/com/codecool/weetter/WeetterApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.weetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/weetter/WeetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.weetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
